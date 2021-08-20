395921dd4a2f7faf4233f3a938116bddc2840643
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2021-08-20 22:26:30 +0800

    Minor changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         // get the value entered the user in the `<input>` element
         var email = $('#email').val();
         if (email.includes(" ")){
+            $('#email').css('border', '2px solid red');
             $('#emailError').text('Email is invalid');
             $('#submit').css('background-color', 'grey');
             $('#submit').prop('disabled', true);
@@ -12,14 +13,14 @@ $(document).ready(function () {
             $.get('/getCheckEmail', {email: email}, function (result) {
                 
                 if(result.email == email) {
-                    $('#email').css('border', 'red');
+                    $('#email').css('border', '2px solid red');
                     $('#emailError').text('Email already registered');
                     $('#submit').css('background-color', 'grey');
                     $('#submit').prop('disabled', true);
                 }
                 
                 else {
-                    $('#email').css('border', 'white');
+                    $('#email').css('border', '');
                     $('#emailError').text('');
                     $('#submit').css('background-color', '#0275d8');
                     $('#submit').prop('disabled', false);
@@ -33,11 +34,13 @@ $(document).ready(function () {
 
         if (password.length < 8) {
             $('#passwordError').text('Password must contain at least 8 characters');
+            $('#password').css('border', '2px solid red');
             $('#submit').css('background-color', 'grey');
             $('#submit').prop('disabled', true);
         }
         else {
             $('#passwordError').text('');
+            $('#password').css('border', '');
             $('#submit').css('background-color', '#0275d8');
             $('#submit').prop('disabled', false);
         }
