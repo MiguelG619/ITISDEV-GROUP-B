927382e64455bbec53c4f6f7cc571e944a1a7e27
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2021-08-21 17:46:12 +0800

    Implement hashing and server-side validation

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 
 const db = require('../models/db.js');
 const User = require('../models/UserModel.js');
@@ -10,6 +11,32 @@ const loginController = {
 
     postLogIn: function (req, res) {
 
+        var email = req.body.email;
+        var password = req.body.password;
+
+        db.findOne(User, {email: email}, '', function(result) {
+            if(result) {
+                var user = {
+                    firstName: result.firstName,
+                    lastName: result.lastName,
+                    email: result.email,
+                    role: result.role
+                }
+
+                bcrypt.compare(password, result.password, function(err, equal){
+                    if(equal)
+                        res.render('index');
+                    else {
+                        var details = {error: 'Incorrect Email or Password.'};
+                        res.render('login', details);
+                    }
+                });
+            }
+            else {
+                var details = {error: 'Incorrect Email or Password.'};
+                res.render('login', details);
+            }
+        });
     }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -5,10 +5,13 @@ const controller = require('../controllers/controller.js');
 const loginController = require('../controllers/loginController.js');
 const signupController = require('../controllers/signupController.js');
 
+const validation = require('../helpers/validation.js');
+
 router.get('/', controller.getIndex);
 router.get('/login', loginController.getLogIn);
+router.post('/login', loginController.postLogIn);
 router.get('/signup', signupController.getSignUp);
-router.post('/signup', signupController.postSignUp);
+router.post('/signup', validation.signupValidation(), signupController.postSignUp);
 router.get('/getCheckEmail', signupController.getCheckEmail);
 
 
